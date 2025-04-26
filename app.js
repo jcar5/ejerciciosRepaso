75b8b7a667d3d7c6e7c5b40e28a00cf85280bec2
jcar512 <110567627+jcar512@users.noreply.github.com>
2025-04-26 00:26:14 -0300

    uncomment

--- app.js
+++ app.js
@@ -1,75 +1,75 @@
 //1. Variables: let y const
-//let nombre = 'Joaquin';
-//const edad = '30';
+let nombre = 'Joaquin';
+const edad = '30';
 
-//console.log(`Hola, mi nombre es ${nombre} y tengo ${edad} años.\n`);
+console.log(`Hola, mi nombre es ${nombre} y tengo ${edad} años.\n`);
 
-////2. Estructuras y tipos de datos
-//const producto = {
-//	nombre: 'Arroz',
-//	precio: 50,
-//	enStock: true,
-//};
+//2. Estructuras y tipos de datos
+const producto = {
+	nombre: 'Arroz',
+	precio: 50,
+	enStock: true,
+};
 
-//console.log(`Producto: ${producto.nombre}\nPrecio:${producto.precio}\n`);
+console.log(`Producto: ${producto.nombre}\nPrecio:${producto.precio}\n`);
 
 //3. Condicionales (if / else)
-// const readline = require('readline');
-// const rl = readline.createInterface({
-// 	input: process.stdin,
-// 	output: process.stdout,
-// });
+/* const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
 
-// rl.question('Ingresa tu edad: ', (answer) => {
-// 	if (parseInt(answer) >= 18) {
-// 		console.log('Eres mayor de edad.');
-// 	} else if (parseInt(answer) < 18) {
-// 		console.log('Eres menor de edad');
-// 	} else {
-// 		console.log('Debes ingresar un número válido');
-// 	}
+rl.question('Ingresa tu edad: ', (answer) => {
+	if (parseInt(answer) >= 18) {
+		console.log('Eres mayor de edad.');
+	} else if (parseInt(answer) < 18) {
+		console.log('Eres menor de edad');
+	} else {
+		console.log('Debes ingresar un número válido');
+	}
 
-// 	rl.close();
-// });
+	rl.close();
+}); */
 
 //4. Arreglos y objetos
-// const arr = ['Juan', 'Pepe', 'Andres'];
+const arr = ['Juan', 'Pepe', 'Andres'];
 
-// const objeto = {
-// 	amigos: [arr],
-// };
+const objeto = {
+	amigos: [arr],
+};
 
 //5. Funciones y arrow functions
-//console.log(objeto.amigos[0]);
+console.log(objeto.amigos[0]);
 
-//function suma(num1, num2) {
-//	return num1 + num2;
-//}
+function suma(num1, num2) {
+	return num1 + num2;
+}
 
-//console.log(suma(1, 2) + '\n');
+console.log(suma(1, 2) + '\n');
 
-//const sumaFlecha = (num1, num2) => {
-//	return num1 + num2;
-//};
+const sumaFlecha = (num1, num2) => {
+	return num1 + num2;
+};
 
-//console.log(sumaFlecha(2, 3) + '\n');
+console.log(sumaFlecha(2, 3) + '\n');
 
-////6. Selectores de DOM / Eventos del DOM
-//const button = document.querySelector('#button');
+//6. Selectores de DOM / Eventos del DOM
+const button = document.querySelector('#button');
 
-//button.addEventListener('click', () => {
-//	console.log('¡Botón presionado!');
-//});
+button.addEventListener('click', () => {
+	console.log('¡Botón presionado!');
+});
 
 //7. Fetch API y promesas
-// async function fetchData() {
-// 	const data = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+async function fetchData() {
+	const data = await fetch(`https://jsonplaceholder.typicode.com/posts`);
 
-// 	return await data.json();
-// }
+	return await data.json();
+}
 
-// fetchData().then((res) => {
-// 	console.log(
-// 		`Titulo 1: ${res[0].title}\nTitulo 2: ${res[1].title}\nTitutlo 3: ${res[2].title}`
-// 	);
-// });
+fetchData().then((res) => {
+	console.log(
+		`Titulo 1: ${res[0].title}\nTitulo 2: ${res[1].title}\nTitutlo 3: ${res[2].title}`
+	);
+});
